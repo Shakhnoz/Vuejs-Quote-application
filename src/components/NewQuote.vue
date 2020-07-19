3fb14665af98594393ddf64395f95fe2fd95ebd1
Shakhnoza Karimova <shahnozkarimova1997@gmail.com>
2020-07-19 11:18:21 +0500

    quote input aligned center

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="row">
+  <div class="row d-flex justify-content-center">
     <form>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
         <label>Quote</label>
-        <textarea id="textarea" class="form-control" cols="60" rows="3" v-model="quote"></textarea>
+        <textarea id="textarea" cols="60" rows="3" v-model="quote"></textarea>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
         <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
